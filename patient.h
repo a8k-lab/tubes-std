1ec3796a201d1347347c4400b3108f763df8b4fd
Mandarizka <64694325+mandarizka@users.noreply.github.com>
2024-12-01 16:01:10 +0700

    Update patient.h

--- patient.h
+++ patient.h
@@ -27,5 +27,42 @@ struct ListPatient {
 // IS. -
 // FS. Terbentuk sebuah list di mana, first dari L bernilai NIL.
 
+void createdNewListPatient(ListPatient &L);
+
+// Fungsi mengembalikan elemen patient berdasarkan parameter fungsi, yaitu 'Elm'.
+patientAddress allocatePatient(patientInfo Elm);
+
+// IS. Terdefinisi pointer P berisi alamat elmPatient, dan seuah list L ( L mungkin kosong).
+// FS. elmPatient yang ditunjuk oleh P ditambahkan ke dalam list sebagai elemen terakhir.
+
+void insertPatientLast(ListPatient &L, patientAddress P);
+
+// IS. Terdefinisi sebuah list L (L tidak kosong dan mungkin berisi satu elemen).
+// FS. P berisi alamat elmPatient yang terakhir, elmPatient yang ditunjuk oleh P dihapus dari list
+void deletePatient(ListPatient &L, string patientInfo);
+
+// Fungsi untuk mencari pasien berdasarkan info
+patientAddress findPatient(ListPatient &L, string patientInfo);
+
+// Fungsi mengembalikan True apabila list kosong, atau False bila sebaliknya.
+bool isPatientEmpty(ListPatient L);
+
+// IS. Terdefinisi sebuah list patient L
+// FS. Menampilkan semua info elmPatient di list.
+void printListPatient(ListPatient L);
+
+// IS. Terdefinisi sebuah list patient L
+// FS. Menampilkan elmPatient yang ditangani oleh dokter tertentu.
+void printListPatientByDoctor(ListPatient L, string doctor);
+
+// IS. Terdefinisi sebuah list patient L
+// FS. Menampilkan detail pasien tertentu beserta dokter yang menanganinya.
+void printPatientDetails(ListPatient L, string patientInfo);
+
+// Fungsi untuk menghitung total dokter yang menangani pasien tertentu
+int countDoctorsForPatient(ListPatient L, string patientInfo);
+
+// Fungsi untuk menghitung total pasien yang belum ditangani dokter
+int countPatientNoDoctor(ListPatient L);
 
 #endif // PATIENT_H_INCLUDED
