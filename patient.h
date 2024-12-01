90913768428dfff7f27a420af73b3ac0d108adfc
abdul <up2dul@gmail.com>
2024-12-01 14:52:25 +0700

    feat: define required structs and types

--- patient.h
+++ patient.h
@@ -1,6 +1,27 @@
 #ifndef PATIENT_H_INCLUDED
 #define PATIENT_H_INCLUDED
+#include <iostream>
+#include "treatment.h"
 
+typedef struct Patient patientInfo;
+typedef struct ElmPatient *patientAddress;
 
+struct Patient {
+  string info;
+  string gender;
+  int age;
+};
+struct ElmPatient {
+  patientInfo info;
+  patientAddress prev;
+  patientAddress next;
+  treatmentAddress relation;
+};
+struct ListPatient {
+  patientAddress first;
+  patientAddress last;
+};
+
+// TODO: required procedures and functions below here
 
 #endif // PATIENT_H_INCLUDED
