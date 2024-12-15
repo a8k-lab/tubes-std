b6fdcf16657bec1a5fb52a8bcf8d55cde2d667dc
Mandarizka <64694325+mandarizka@users.noreply.github.com>
2024-12-15 17:13:01 +0700

    Update menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -16,9 +16,13 @@ int selectMenu() {
 int selectDoctorMenu() (
     cout << "====== MENU DOKTER =======" << endl;
     cout << "1. Tambah Dokter" << endl;
-    cout << "2. Lihat Daftar Dokter" << endl;
-    cout << "3. Cari Dokter" << endl;
-    cout << "4. Hapus Data Dokter" << endl;
+    cout << "2. Hapus Dokter" << endl;
+    cout << "3. Mencari Dokter" << endl;
+    cout << "4. Menampilkan Semua Data Dokter" << endl;
+    cout << "5. Menampilkan Dokter Berdasarkan Pasien" << endl;
+    cout << "6. Menampilkan Dokter, Pasien, dan Perawatannya" << endl;
+    cout << "7. Menampilkan Total Pasien pada Dokter Tertentu" << endl;
+    cout << "8. Menampilkan Total Dokter yang Tidak Memiliki Pasien" << endl;
     cout << "0. Kembali ke Menu Utama" << endl;
     cout << "Pilih menu: ";
 
@@ -29,8 +33,8 @@ int selectDoctorMenu() (
 int selectTreatmentMenu() (
     cout << "====== MENU PERAWATAN =======" << endl;
     cout << "1. Tambah Data Perawatan" << endl;
-    cout << "2. Edit Dokter atau Pasien dari Data Perawatan" << endl;
-    cout << "3. Lihat Data Perawatan" << endl;
+    cout << "2. Hapus Data Perawatan" << endl;
+    cout << "3. Edit Dokter atau Pasien dari Data Perawatan" << endl;
     cout << "0. Kembali ke Menu Utama" << endl;
     cout << "Pilih menu: ";
 
@@ -41,8 +45,13 @@ int selectTreatmentMenu() (
 int selectPatientMenu(); {
     cout << "====== MENU PASIEN =======" << endl;
     cout << "1. Tambah Pasien" << endl;
-    cout << "2. Lihat Daftar Pasien" << endl;
+    cout << "2. Menghapus Data Pasien" << endl;
     cout << "3. Cari Pasien" << endl;
+    cout << "4. Menampilkan Semua Data Pasien" << endl;
+    cout << "5. Menampilkan Pasien Berdasarkan Dokter" << endl;
+    cout << "6. Menampilkan Pasien, Dokter, dan Perawatannya" << endl;
+    cout << "7. Menampilkan Total Dokter yang Menangani Pasien Tertentu" << endl;
+    cout << "8. Menampilkan Total Pasien yang Belum Ditangani Dokter" << endl;
     cout << "0. Kembali ke Menu Utama" << endl;
     cout << "Pilih menu: ";
 
