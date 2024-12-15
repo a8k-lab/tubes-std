427e09f121624124d356989f81c50307ae3826fd
Mandarizka <64694325+mandarizka@users.noreply.github.com>
2024-12-15 16:27:56 +0700

    Update menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -12,3 +12,41 @@ int selectMenu() {
     cin >> input;
     return input;
 }
+
+int selectDoctorMenu() (
+    cout << "====== MENU DOKTER =======" << endl;
+    cout << "1. Tambah Dokter" << endl;
+    cout << "2. Lihat Daftar Dokter" << endl;
+    cout << "3. Cari Dokter" << endl;
+    cout << "4. Hapus Data Dokter" << endl;
+    cout << "0. Kembali ke Menu Utama" << endl;
+    cout << "Pilih menu: ";
+
+    int input = 0;
+    cin >> input;
+    return input
+)
+int selectTreatmentMenu() (
+    cout << "====== MENU PERAWATAN =======" << endl;
+    cout << "1. Tambah Data Perawatan" << endl;
+    cout << "2. Edit Dokter atau Pasien dari Data Perawatan" << endl;
+    cout << "3. Lihat Data Perawatan" << endl;
+    cout << "0. Kembali ke Menu Utama" << endl;
+    cout << "Pilih menu: ";
+
+    int input = 0;
+    cin >> input;
+    return input
+)
+int selectPatientMenu(); {
+    cout << "====== MENU PASIEN =======" << endl;
+    cout << "1. Tambah Pasien" << endl;
+    cout << "2. Lihat Daftar Pasien" << endl;
+    cout << "3. Cari Pasien" << endl;
+    cout << "0. Kembali ke Menu Utama" << endl;
+    cout << "Pilih menu: ";
+
+    int input = 0;
+    cin >> input;
+    return input
+}
